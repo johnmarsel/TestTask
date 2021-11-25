4321c7c57dd70453efca5d649ad69003555f9f67
johnmarsel <vlad.gorbachyov@gmail.com>
2021-11-25 18:46:17 +0300

    Added Navigation Component

---
+++ app/src/main/java/com/johnmarsel/testtask/AlbumFragment.kt
@@ -0,0 +1,24 @@
+package com.johnmarsel.testtask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.viewbinding.ViewBinding
+import com.johnmarsel.testtask.databinding.FragmentAlbumBinding
+
+class AlbumFragment: Fragment() {
+
+    lateinit var binding: FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
